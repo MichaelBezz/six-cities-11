738e023c9473bc87204e77bb6be71cf543cec209
Michael Bezzaponnyy <michael.bezzaponnyy@gmail.com>
2023-02-22 13:46:44 +0300

    Добавит в роутинг FavoritePage

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,9 +1,13 @@
 import {Routes, Route} from 'react-router-dom';
 import {HelmetProvider} from 'react-helmet-async';
+
+import PrivateRoute from '../private-route/private-route';
 import MainPage from '../../pages/main-page/main-page';
 import LoginPage from '../../pages/login-page/login-page';
 import OfferPage from '../../pages/offer-page/offer-page';
+import FavoritePage from '../../pages/favorite-page/favorite-page';
 import NotFoundPage from '../../pages/not-found-page/not-found-page';
+
 import {AppRoute} from '../../constants';
 
 function App(): JSX.Element {
@@ -21,6 +25,10 @@ function App(): JSX.Element {
         <Route
           path={`${AppRoute.Offer}/:id`}
           element={<OfferPage />}
+        />
+        <Route
+          path={AppRoute.Favorite}
+          element={<PrivateRoute privateComponent={<FavoritePage />} />}
         />
         <Route
           path={AppRoute.NotFound}
